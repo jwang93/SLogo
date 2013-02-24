fc5a5c320679cac50e08610b5114b09e8939abf9
Zhen Gou <zg20@duke.edu>
2013-02-24 17:12:56 -0500

    added fileMenu, Clear Button

--- src/view/View.java
+++ src/view/View.java
@@ -1,3 +1,5 @@
+package view;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -10,16 +12,29 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.ResourceBundle;
 
+import javax.swing.AbstractAction;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import model.IModel;
 import model.Model;
@@ -35,12 +50,15 @@ public class View extends JFrame implements IView {
 	private JTextField myTextField;
 	private JFileChooser myChooser;
 	private ResourceBundle myResources;
-	private ActionListener myActionListener;
 	private KeyListener myKeyListener;
 	private FocusListener myFocusListener;
-	private JComponent myCanvas;
+
+	private MouseListener myMouseListener;
+
+	private JButton myClearButton;
 
 	private IModel myModel;
+	private JComponent myCanvas;
 
 	public View(String title, IModel model) {
 		setTitle(title);
@@ -50,8 +68,12 @@ public class View extends JFrame implements IView {
 		myChooser = new JFileChooser(System.getProperties().getProperty(
 				USER_DIR));
 		makeListeners();
+		getContentPane().add(makeCommandCenter(), BorderLayout.SOUTH);
+		getContentPane().add(new JSeparator());
 		getContentPane().add(makeCommandHistory(), BorderLayout.WEST);
-		getContentPane().add(makeTextField(), BorderLayout.SOUTH);
+		getContentPane().add(new JSeparator());
+		setJMenuBar(makeMenuBar());
+
 		getContentPane().add(myCanvas, BorderLayout.CENTER);
 
 		pack();
@@ -65,46 +87,172 @@ public class View extends JFrame implements IView {
 	}
 
 	private JComponent makeCommandHistory() {
+		JPanel result = new JPanel();
+		result.setLayout(new BorderLayout());
+		result.add(new JLabel("Command History"), BorderLayout.NORTH);
 		myTextArea = new JTextArea(FIELD_SIZE, FIELD_SIZE);
 		myTextArea.setEditable(false);
-		return new JScrollPane(myTextArea);
+		JScrollPane scrollPane = new JScrollPane(myTextArea);
+		result.add(scrollPane);
+
+		return result;
 
 	}
 
-	protected JTextField makeTextField() {
-		myTextField = new JTextField(FIELD_SIZE);
+	private JComponent makeCommandCenter() {
+		JComponent result = new JPanel();
+		result.add(new JLabel("Command-Line"));
+		result.add(makeTextField());
+		result.add(new JSeparator());
+		result.add(makeClearButton());
+		return result;
+	}
+
+	private JTextField makeTextField() {
+		myTextField = new JTextField(FIELD_SIZE * 2);
 		myTextField.addKeyListener(myKeyListener);
 		// result.addFocusListener(myFocusListener);
-		myTextField.addActionListener(myActionListener);
+		myTextField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String givenCommand = myTextField.getText();
+				showMessage("Enter pressed" + "  the given Command is:  "
+						+ givenCommand);
+				// myModel.executeCommand(givenCommand);
+				myTextField.setText("");
+
+			}
+		});
 		return myTextField;
+	}
+
+	private JMenuBar makeMenuBar() {
+		JMenuBar result = new JMenuBar();
+		result.add(makeFileMenu());
+		return result;
+
+	}
+
+	protected JMenu makeFileMenu() {
+		JMenu result = new JMenu("File");
+		result.add(new AbstractAction("LoadCommand") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int response = myChooser.showOpenDialog(null);
+					if (response == JFileChooser.APPROVE_OPTION) {
+						File file = myChooser.getSelectedFile();
+
+						myModel.loadFunctionsAndVariables(file);
+						showMessage("file loaded:  " + file.getName());
+						echo(new FileReader(file));
+					}
+				} catch (IOException io) {
+					// let user know an error occurred, but keep going
+					showMessage(io.toString());
+				}
+			}
+		});
+		result.add(new AbstractAction("saveCommand") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				try {
+					int response = myChooser.showOpenDialog(null);
+					if (response == JFileChooser.APPROVE_OPTION) {
+						File file = myChooser.getSelectedFile();
+
+						myModel.saveFunctionsAndVariables(file);
+						showMessage("file saved at:  " + file.getName());
+						echo(new FileReader(file));
+					}
+				} catch (IOException io) {
+					// let user know an error occurred, but keep going
+					showMessage(io.toString());
+				}
+			}
+		});
+		result.add(new JSeparator());
+		result.add(new AbstractAction("Quit") {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// clean up any open resources, then
+				// end program
+				System.exit(0);
+			}
+		});
+		return result;
+	}
+
+	private JButton makeClearButton() {
+		myClearButton = new JButton("Clear");
+		myClearButton.addMouseListener(myMouseListener);
+		myClearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clearCommandWindow();
+			}
+		});
+		myClearButton.setActionCommand("clear");
+		return myClearButton;
+
+	}
+
+	private void echo(Reader r) {
+		try {
+			String s = "";
+			BufferedReader input = new BufferedReader(r);
+			String line = input.readLine();
+			while (line != null) {
+				s += line + "\n";
+				line = input.readLine();
+			}
+			showMessage(s);
+		} catch (IOException e) {
+			showMessage(e.toString());
+		}
 	}
 
 	protected void makeListeners() {
 		myKeyListener = new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent e) {
-				// echo("pressed", e);
 			}
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if (e.getKeyCode() == e.VK_ENTER) {
-					String givenCommand = myTextField.getText();
-					showMessage("Enter pressed" + "  the given Command is:  "
-							+ givenCommand);
-					// myModel.executeCommand(givenCommand);
-					myTextField.setText("");
-				}
-				// echo("released", e);
+
 			}
 
 			@Override
 			public void keyTyped(KeyEvent e) {
 
-				// echo("typed", e);
 			}
 		};
-		// listener for low-level mouse events
+		myMouseListener = new MouseListener() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+
+			}
+		};
 
 	}
 
@@ -128,7 +276,7 @@ public class View extends JFrame implements IView {
 	}
 
 	public void setModel(IModel model) {
-		myModel=model;
+		myModel = model;
 	}
 
 	public static void main(String[] args) {
