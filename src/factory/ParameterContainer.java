5b230b239ebcb2f80dbe4e6b7b6779a08f7df7b8
Will Nance <wllnance@gmail.com>
2013-03-17 15:41:17 -0400

    safety sync

---
+++ src/factory/ParameterContainer.java
@@ -0,0 +1,36 @@
+package factory;
+import java.util.*;
+import commands.ICommand;
+
+/**
+ * This is a container class for the data structure in which we store a command's parameters
+ * It has limited functionality but provides the flexibility to extend behaivor if the 
+ * specifications of the parameters change in some way.  
+ * </br>
+ * 
+ *  This is used as the intermediary class which is passed from an initializer class to the command
+ *  which it is initializing. The objects stored in this list will be removed and used at runtime.
+ * @author Will Nance
+ *
+ */
+public class ParameterContainer {
+    
+    private List <ICommand> parameters = new ArrayList<ICommand>();
+    
+    public ParameterContainer(){
+    }
+    
+    /**remove and return the first element
+     * 
+     * @return the first element
+     */
+   public ICommand remove(){
+       return parameters.remove(0);
+   }
+   public void add(ICommand command){
+       parameters.add(command);
+   }
+   public ICommand get(int i){
+       return parameters.get(i);
+   }
+}
---
+++ src/util/AbstractSprite.java
@@ -0,0 +1,18 @@
+package util;
+
+import java.awt.Graphics2D;
+import java.util.Observable;
+
+
+public abstract class AbstractSprite extends Observable implements Paintable {
+
+    public AbstractSprite () {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void paint (Graphics2D pen) {
+
+    }
+
+}
