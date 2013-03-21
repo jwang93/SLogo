9acc9813ce0ebdca78242dc6fee300487fc5b49d
Zhen Gou <zg20@duke.edu>
2013-03-20 20:30:57 -0400

    **Added create new workspace in Menu**

--- src/view/View.java
+++ src/view/View.java
@@ -35,7 +35,6 @@ import model.IModel;
 import util.DataSource;
 import util.Location;
 
-
 /**
  * The View for this simulation. Contains a Canvas to draw sprites on,
  * information about positions and heading of the turtle, a command window with
@@ -46,226 +45,233 @@ import util.Location;
  * 
  */
 public class View extends JFrame implements Observer {
-    private static final long serialVersionUID = 401L;
-    private static final String DEFAULT_RESOURCE_PACKAGE = "view.resources.";
-    private static final String USER_DIR = "user.dir";
-    private static final JFileChooser FILE_CHOOSER = new JFileChooser(System
-            .getProperties().getProperty(USER_DIR));
-    
-    private JTabbedPane myTabbedPane; 
-    private ResourceBundle myResources;
-    private IModel myModel;
-    private DataSource myDataSource;
-    private int numberOfWorkspaces=0;
-    private String myLanguage;
-    private Dimension myCanvasBounds;
-    
-    private ArrayList<WorkspaceInView> myWorkspaces=new ArrayList<WorkspaceInView>();
+	private static final long serialVersionUID = 401L;
+	private static final String DEFAULT_RESOURCE_PACKAGE = "view.resources.";
+	private static final String USER_DIR = "user.dir";
+	private static final JFileChooser FILE_CHOOSER = new JFileChooser(System
+			.getProperties().getProperty(USER_DIR));
 
-    /**
-     * Creates the view window.
-     * 
-     * @param title
-     *        title of window
-     * @param language
-     *        localization language for configuration file
-     * @param model
-     *        IModel used to communicate with model
-     * @param canvasBounds
-     *        bounds of the canvas
-     */
-    public View (String title, String language, IModel model,
-                 Dimension canvasBounds) {
-        setTitle(title);
-        myCanvasBounds=canvasBounds;
-        myLanguage=language;
-        myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE
-                                               + myLanguage);
-        myModel = model;
-        model.initializeObserver(this);
-        myDataSource = model.getDataSource();
-        myTabbedPane = new JTabbedPane();
-        getContentPane().add(myTabbedPane);
-        createNewWorkspace();
-        createNewWorkspace();
+	private JTabbedPane myTabbedPane;
+	private ResourceBundle myResources;
+	private IModel myModel;
+	private DataSource myDataSource;
+	private int numberOfWorkspaces = 0;
+	private String myLanguage;
+	private Dimension myCanvasBounds;
 
-        setJMenuBar(makeMenuBar());
-        setComponentListener();
-        setTabChangeListener();
+	private ArrayList<WorkspaceInView> myWorkspaces = new ArrayList<WorkspaceInView>();
 
-        pack();
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	/**
+	 * Creates the view window.
+	 * 
+	 * @param title
+	 *            title of window
+	 * @param language
+	 *            localization language for configuration file
+	 * @param model
+	 *            IModel used to communicate with model
+	 * @param canvasBounds
+	 *            bounds of the canvas
+	 */
+	public View(String title, String language, IModel model,
+			Dimension canvasBounds) {
+		setTitle(title);
+		myCanvasBounds = canvasBounds;
+		myLanguage = language;
+		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE
+				+ myLanguage);
+		myModel = model;
+		model.initializeObserver(this);
+		myDataSource = model.getDataSource();
+		myTabbedPane = new JTabbedPane();
+		getContentPane().add(myTabbedPane);
+		createNewWorkspace();
 
-    }
+		setJMenuBar(makeMenuBar());
+		setComponentListener();
+		setTabChangeListener();
 
-    /**
-     * creates a new workspace, update no. of workspaces, my workspace arraylist
-     */
-    public void createNewWorkspace(){
-    	numberOfWorkspaces++;
-    	WorkspaceInView workspace=new WorkspaceInView(myModel, myCanvasBounds,myLanguage, numberOfWorkspaces);
-    	myTabbedPane.addTab("Workspace "+numberOfWorkspaces, workspace);
-    	myWorkspaces.add(workspace);
-    	
-    }
+		pack();
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	}
 
-    /**
-     * Creates the menu bar.
-     * 
-     * @return JMenuBar representing the menu bar
-     */
-    private JMenuBar makeMenuBar () {
-        JMenuBar result = new JMenuBar();
-        result.add(makeFileMenu());
-        return result;
+	/**
+	 * creates a new workspace, update no. of workspaces, my workspace arraylist
+	 */
+	public void createNewWorkspace() {
+		numberOfWorkspaces++;
+		WorkspaceInView workspace = new WorkspaceInView(myModel,
+				myCanvasBounds, myLanguage, numberOfWorkspaces);
+		myTabbedPane.addTab("Workspace " + numberOfWorkspaces, workspace);
+		myWorkspaces.add(workspace);
 
-    }
+	}
 
-    /**
-     * Creates a menu with 3 options: Save, Load, and Exit.
-     * 
-     * @return JMenu representing the menu.
-     */
-    @SuppressWarnings("serial")
-    protected JMenu makeFileMenu () {
-        JMenu result = new JMenu(myResources.getString("File"));
-        result.add(new AbstractAction(myResources.getString("LoadCommand")) {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                int response = FILE_CHOOSER.showOpenDialog(null);
-                if (response == JFileChooser.APPROVE_OPTION) {
-                    File file = FILE_CHOOSER.getSelectedFile();
+	/**
+	 * Creates the menu bar.
+	 * 
+	 * @return JMenuBar representing the menu bar
+	 */
+	private JMenuBar makeMenuBar() {
+		JMenuBar result = new JMenuBar();
+		result.add(makeFileMenu());
+		return result;
 
-                    myModel.loadFunctionsAndVariables(file);
-                    showMessage(myResources.getString("FileLoaded")
-                                + file.getName());
+	}
 
-                }
-            }
-        });
-        result.add(new AbstractAction(myResources.getString("SaveCommand")) {
-            @Override
-            public void actionPerformed (ActionEvent e) {
+	/**
+	 * Creates a menu with 3 options: Save, Load, and Exit.
+	 * 
+	 * @return JMenu representing the menu.
+	 */
+	@SuppressWarnings("serial")
+	protected JMenu makeFileMenu() {
+		JMenu result = new JMenu(myResources.getString("File"));
+		result.add(new AbstractAction(myResources.getString("new_Workspace")) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				createNewWorkspace();
+			}
+		});
+		result.add(new AbstractAction(myResources.getString("LoadCommand")) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int response = FILE_CHOOSER.showOpenDialog(null);
+				if (response == JFileChooser.APPROVE_OPTION) {
+					File file = FILE_CHOOSER.getSelectedFile();
 
-                int response = FILE_CHOOSER.showSaveDialog(null);
-                if (response == JFileChooser.APPROVE_OPTION) {
-                    File file = FILE_CHOOSER.getSelectedFile();
+					myModel.loadFunctionsAndVariables(file);
+					showMessage(myResources.getString("FileLoaded")
+							+ file.getName());
 
-                    myModel.saveFunctionsAndVariables(file);
-                    showMessage(myResources.getString("FileSaved")
-                                + file.getName());
+				}
+			}
+		});
+		result.add(new AbstractAction(myResources.getString("SaveCommand")) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
 
-                }
-            }
-        });
-        result.add(new AbstractAction(myResources.getString("Quit")) {
-            @Override
-            public void actionPerformed (ActionEvent e) {
-                // clean up any open resources, then end program
-                System.exit(0);
-            }
-        });
-        return result;
-    }
+				int response = FILE_CHOOSER.showSaveDialog(null);
+				if (response == JFileChooser.APPROVE_OPTION) {
+					File file = FILE_CHOOSER.getSelectedFile();
 
-    /**
-     * Writes the message to the command window.
-     * 
-     * @param message
-     */
-    private void showMessage (String message) {
-    	getCurrentWorkspace().showMessage(message);
-    }
+					myModel.saveFunctionsAndVariables(file);
+					showMessage(myResources.getString("FileSaved")
+							+ file.getName());
 
-    /**
-     * Updates the position label with the new location.
-     * 
-     * @param location
-     */
-    private void updatePositionLabel (Location location) {
-    	getCurrentWorkspace().updatePositionLabel(location);
-    }
-    
-    private WorkspaceInView getCurrentWorkspace(){
-    	int currentWorkspace=myTabbedPane.getSelectedIndex();
-    	return myWorkspaces.get(currentWorkspace);
-    	
-    }
+				}
+			}
+		});
+		result.add(new AbstractAction(myResources.getString("Quit")) {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// clean up any open resources, then end program
+				System.exit(0);
+			}
+		});
+		return result;
+	}
 
-    /**
-     * updates the heading label with the new heading.
-     * 
-     * @param heading
-     */
-    private void updateHeadingLabel (int heading) {
-    	getCurrentWorkspace().updateHeadingLabel(heading);
+	/**
+	 * Writes the message to the command window.
+	 * 
+	 * @param message
+	 */
+	private void showMessage(String message) {
+		getCurrentWorkspace().showMessage(message);
+	}
 
-    }
+	/**
+	 * Updates the position label with the new location.
+	 * 
+	 * @param location
+	 */
+	private void updatePositionLabel(Location location) {
+		getCurrentWorkspace().updatePositionLabel(location);
+	}
 
-    /**
-     * Overrides the default Obeserver method. When called it updates canvas
-     * with the new paintable iterator and then updates all labels and shows the
-     * return value.
-     * 
-     * @param arg0
-     *        Object passed in to observer, unused in this class
-     * @param arg1
-     *        Object passed in to observer, unused in this class
-     */
-    @Override
-    public void update (Observable arg0, Object arg1) {
-    	getCurrentWorkspace().update();
-        /*myCanvas.update(myDataSource.getPaintableIterator(), myDataSource.getBackgroundImage());
-        updateHeadingLabel(myDataSource.getTurtleHeading());
-        updatePositionLabel(myDataSource.getTurtlePosition());
-        showMessage("" + myDataSource.getReturnValue());
-        showMessage(myDataSource.showMessage());*/
+	private WorkspaceInView getCurrentWorkspace() {
+		int currentWorkspace = myTabbedPane.getSelectedIndex();
+		return myWorkspaces.get(currentWorkspace);
 
-    }
+	}
 
-    /**
-     * Add component listener to the GUI so that when window size changed the canvas will update,
-     * repainting objects;
-     */
+	/**
+	 * updates the heading label with the new heading.
+	 * 
+	 * @param heading
+	 */
+	private void updateHeadingLabel(int heading) {
+		getCurrentWorkspace().updateHeadingLabel(heading);
 
-    public void setComponentListener () {
-        addComponentListener(new ComponentListener() {
-            @Override
-            public void componentResized (ComponentEvent evt) {
-               getCurrentWorkspace().update();
+	}
 
-            }
+	/**
+	 * Overrides the default Obeserver method. When called it updates canvas
+	 * with the new paintable iterator and then updates all labels and shows the
+	 * return value.
+	 * 
+	 * @param arg0
+	 *            Object passed in to observer, unused in this class
+	 * @param arg1
+	 *            Object passed in to observer, unused in this class
+	 */
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		getCurrentWorkspace().update();
+		/*
+		 * myCanvas.update(myDataSource.getPaintableIterator(),
+		 * myDataSource.getBackgroundImage());
+		 * updateHeadingLabel(myDataSource.getTurtleHeading());
+		 * updatePositionLabel(myDataSource.getTurtlePosition()); showMessage(""
+		 * + myDataSource.getReturnValue());
+		 * showMessage(myDataSource.showMessage());
+		 */
 
-            @Override
-            public void componentHidden (ComponentEvent arg0) {
+	}
 
-            }
+	/**
+	 * Add component listener to the GUI so that when window size changed the
+	 * canvas will update, repainting objects;
+	 */
 
-            @Override
-            public void componentMoved (ComponentEvent arg0) {
+	public void setComponentListener() {
+		addComponentListener(new ComponentListener() {
+			@Override
+			public void componentResized(ComponentEvent evt) {
+				getCurrentWorkspace().update();
 
-            }
+			}
 
-            @Override
-            public void componentShown (ComponentEvent arg0) {
+			@Override
+			public void componentHidden(ComponentEvent arg0) {
 
+			}
 
-            }
-        });
-    }
-    
-    /**
-     * monitor tab change; if changed update, to ensure repainting
-     */
-    public void setTabChangeListener(){
-    	myTabbedPane.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-              System.out.println("Tab=" + myTabbedPane.getSelectedIndex());
-              getCurrentWorkspace().update();
-            }
-          });
-    }
+			@Override
+			public void componentMoved(ComponentEvent arg0) {
+
+			}
+
+			@Override
+			public void componentShown(ComponentEvent arg0) {
+
+			}
+		});
+	}
+
+	/**
+	 * monitor tab change; if changed update, to ensure repainting
+	 */
+	public void setTabChangeListener() {
+		myTabbedPane.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				System.out.println("Tab=" + myTabbedPane.getSelectedIndex());
+				getCurrentWorkspace().update();
+			}
+		});
+	}
 
 }
