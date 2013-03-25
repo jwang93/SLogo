3de5dd22f034632867109cddff1d1770e6a3336a
Will Nance <wllnance@gmail.com>
2013-03-25 00:04:34 -0400

    fixed tell to use the correct formatting

--- src/commands/Tell.java
+++ src/commands/Tell.java
@@ -3,6 +3,8 @@ package commands;
 import java.util.List;
 import model.IModel;
 
+/*Kinda funky and tricky to understand. The single parameter is actually a list of all of the 
+ * turtle indices (or other commands), but youre not really sure how many you get.*/
 public class Tell extends AbstractSingleParameterCommand implements ICommand {
       
     /**
@@ -11,17 +13,20 @@ public class Tell extends AbstractSingleParameterCommand implements ICommand {
     private static final long serialVersionUID = 1L;
     private IModel myModel;
 
-    public Tell (List<ICommand> parameters, IModel model) {
+    public Tell ( List<ICommand> parameters, IModel model) {
         super(parameters);
         myModel = model;
     }
     
     
     public int execute(){
-        resolveParameters();
-        int turtleIndex = getOnlyParameter();
-        myModel.getTurtle().setActiveTurtles(turtleIndex);
-        return turtleIndex;
+        List<ICommand> listOfTurtleIndices = getCommands();
+        int[] TurtleInts = new int[listOfTurtleIndices.size()];
+        // I hate arrays so much
+        for(int i = 0 ; i< listOfTurtleIndices.size(); i++){
+            TurtleInts[i]=listOfTurtleIndices.get(i).execute();
+        }
+        return myModel.getTurtle().setActiveTurtles(TurtleInts);
     }
     
     
--- src/factory/TellInitializer.java
+++ src/factory/TellInitializer.java
@@ -1,21 +1,54 @@
 package factory;
 
+import java.util.ArrayList;
 import java.util.List;
 import model.Model;
 import commands.Forward;
 import commands.ICommand;
 import commands.Tell;
+import exceptions.FormattingException;
+
 
 public class TellInitializer extends AbstractInitializer {
+
+    private ICommand[] myTurtles;
 
     public TellInitializer (Model model, Parser parser) {
         super(model, parser);
         setNumArgs(Tell.NUM_ARGS);
     }
 
+//    protected ICommand build (CommandStream commandStream) throws FormattingException {
+//        myTurtles = parseTurtles(commandStream);
+//        return super.build(commandStream);
+//    }
+//    private ICommand[] parseTurtles (CommandStream commands) throws FormattingException {
+//        if (!commands.peek().equals("[")) throw new FormattingException();
+//        commands.remove();
+//        List<ICommand> turtles = new ArrayList<ICommand>();
+//        
+//
+//            turtles.add(processParameters(commands));
+//        
+//    }
+    /*This is kind of a hacky way of solving this issue , but it works, and I'm happy with it.*/
+    protected List<ICommand> processParameters (CommandStream commands) throws FormattingException {
+        if(! commands.peek().equals("[")) throw new FormattingException();
+        commands.remove();
+        while (!commands.peek().equals("]")) {
+            int startLength = myParameters.size();
+            processParameter(commands);
+            if (!(myParameters.size() > startLength))
+                throw new FormattingException(String.format("Tell not formatted correctly"));
+        }
+        commands.remove();
+        return myParameters;
+    }
+
     @Override
     protected ICommand instantiate (List<ICommand> parameters) {
-        return new Tell(parameters , getModel());
+
+        return new Tell(parameters, getModel());
     }
 
 }
